80fd82b9e8660a1dbf5e9b9aa4e7c20c4ba51488
Bradley Odell <btodell@hotmail.com>
2020-05-20 01:21:08 -0700

    Added tests. Added CI with Travis.

--- test/unknown.spec.ts
+++ test/unknown.spec.ts
@@ -1,1 +1,30 @@
 import {hasProperty} from "unknown";
+import chai from "chai";
+const expect = chai.expect;
+
+const unknownUndefined: unknown = undefined;
+const unknownNull: unknown = null;
+const unknownFalse: unknown = false;
+const unknownTrue: unknown = true;
+const unknownZero: unknown = 0;
+const unknownNumber: unknown = 42;
+const unknownEmpty: unknown = "";
+const unknownString: unknown = "some string here";
+const unknownFunction: unknown = function() {};
+const unknownArrow: unknown = () => {};
+const unknownEmptyObject: unknown = {};
+const unknownObject: unknown = { something: "string" };
+const unknownNumberObject: unknown = { 0: "number" };
+const unknownSymbolObject: unknown = { [Symbol("name")]: "symbol" };
+
+describe("unknownObject", function() {
+    it("should have 'something'", function() {
+        expect(hasProperty(unknownObject, "something")).to.equal(true);
+    });
+    it("shouldn't have 'nothing'", function() {
+        expect(hasProperty(unknownObject, "nothing")).to.equal(false);
+    });
+    it("shouldn't have zero", function() {
+        expect(hasProperty(unknownObject, 0)).to.equal(false);
+    });
+});
